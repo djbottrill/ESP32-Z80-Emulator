5dbe00b071202d185f3471a9690cad8f7b9de5d9
David Bottrill <david@bottrill.org>
2023-04-15 17:25:03 +0100

    Telnet Improvement
    Drains garbage received via telnet after a new telnet session is initiated

--- ESP32_Z80_Emulator/telnet.h
+++ ESP32_Z80_Emulator/telnet.h
@@ -7,15 +7,17 @@ void TelnetTask(void *parameter) {
   server.begin();
   server.setNoDelay(true);
   Serial.println("Telnet Task Started");
+  Serial.print("Use 'telnet ");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
 
   for (;;) {
 
     if (server.hasClient()) {
       serverClient = server.available();
-      Serial.print("New client: ");
+      Serial.print("\n\rNew Telnet client @ ");
       Serial.println(serverClient.remoteIP());
-      vTaskDelay(500);
-
+    
       serverClient.write(255);  // IAC
       serverClient.write(251);  // WILL
       serverClient.write(1);    // ECHO
@@ -30,7 +32,8 @@ void TelnetTask(void *parameter) {
       vTaskDelay(100);
       serverClient.write(27);   //Print "esc"
       serverClient.print("c");  //Send esc c to reset screen
-      vTaskDelay(500);
+
+      while (serverClient.available()) serverClient.read(); //Get rid of any garbage received      
       RUN = false;              //Force Z80 reboot
     }
    
